bda962aee76d9a1dc3f649f8b0e969b4c6950091
Soner Sayakci <s.sayakci@shopware.com>
2022-11-02 11:26:14 +0100

    nixify setup

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "Recovery";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.simpleFlake {
+      inherit self nixpkgs;
+      name = "shopware-recovery";
+      shell = ./shell.nix;
+    };
+}
